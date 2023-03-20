282accc0170e7d0d118cf295243af11c7a62136c
JC Chuan <jc.chuan.0303@gmail.com>
2023-03-20 23:25:53 +0800

    t2 halfway

--- src/main/java/T2/T2Q1.java
+++ src/main/java/T2/T2Q1.java
@@ -1,2 +1,26 @@
-package T2;public class T2Q1 {
+package T2;
+
+class Container<T> {
+    private T t;
+    public Container(){
+    }
+
+    void add(T t){
+        this.t = t;
+    }
+
+    T retrieve(){
+        return t;
+    }
+
+    public static void main(String[] args) {
+        Container<Integer> c1 = new Container<Integer>();
+        Container<String> c2 = new Container<String>();
+        c1.add(50);
+        c2.add("Java");
+        System.out.println(c1.retrieve());
+        System.out.println(c2.retrieve());
+    }
 }
+
+
--- src/main/java/T2/T2Q2.java
+++ src/main/java/T2/T2Q2.java
@@ -1,2 +1,19 @@
-package T2;public class T2Q2 {
+package T2;
+
+class MyArray {
+    static <E> void listAll(E[] arr) {
+        for (E e : arr) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr1 = {1, 2, 3, 4, 5};
+        String[] arr2 = {"Jane", "Tom", "Bob"};
+        Character[] arr3 = {'a', 'b', 'c'};
+        listAll(arr1);
+        listAll(arr2);
+        listAll(arr3);
+    }
 }

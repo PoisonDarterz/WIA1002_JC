3bd583f491fc8f9b4ed0f547c38d84f92d94ca2e
JC Chuan <jc.chuan.0303@gmail.com>
2023-06-07 21:27:30 +0800

    copy prior queue from fyiernzy

---
+++ src/main/java/L7b/Q1/QueueUtils/Queue.java
@@ -0,0 +1,12 @@
+package L7b.Q1.QueueUtils;
+
+public interface Queue<E> {
+	public String toString();
+	public E poll();
+	public void add(E o);
+	public Object[] toArray();
+	public E peek();
+	public boolean contains(E o);
+	public int size();
+	public boolean isEmpty();
+}
